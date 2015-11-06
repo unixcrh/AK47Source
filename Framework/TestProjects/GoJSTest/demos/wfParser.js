9f2aacaae9c8094939f4dc2103c460271acdf86c
Zheng Shen <shenzheng72@hotmail.com>
2015-11-06 18:51:35 +0800

    创建GoJS测试工程

---
+++ Framework/TestProjects/GoJSTest/demos/wfParser.js
@@ -0,0 +1,28 @@
+﻿if (typeof (wf) == "undefined")
+    wf = {};
+
+wf.nodeElapsedParser = function (v) {
+    var result = "gray";
+
+    switch (v) {
+        case true:
+        case "true":
+            result = "green";
+            break;
+    }
+
+    return result;
+}
+
+wf.returnLinkParser = function (v) {
+    var result = null;
+
+    switch (v) {
+        case true:
+        case "true":
+            result = [5, 5];
+            break;
+    }
+
+    return result;
+}
